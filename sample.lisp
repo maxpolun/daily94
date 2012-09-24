c82a9e361468fbac2ba877fabd3c4387aa3b591d
Max Polun <maxpolun@gmail.com>
2012-09-24 09:37:02 -0400

    clean out some old stuff

--- sample.lisp
+++
@@ -1,8 +0,0 @@
-;
-; this is a sample list file for this implementation of list in go
-;
-
-(print "print works")
-(cond ((> 2 1) (print "cond works")))
-(print (quote quote-works))
-((lambda (s) (print s)) "lambda works")
